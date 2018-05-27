88c8bb05ebbf203874e681480bd2e18e726439c3
Christian Kurz <chrissikurz@gmail.com>
2018-05-27 16:54:15 +0200

    starting locations as per Tetris Guidline (see #1)

--- src/tiles.jl
+++ src/tiles.jl
@@ -10,7 +10,7 @@ mutable struct L <: Tile
     orientation::Int
     mirrored::Bool
 end
-L() = L([5,1], 0, false)
+L() = L([4,1], 0, false)
 data(tile::L) = [1 0 0; 1 1 1] |> x -> rotr90(x, tile.orientation) |>
                 x -> tile.mirrored ? reverse(x * 9 #=orange=#, dims=2) : x * 4 #=blue=#
 
@@ -20,7 +20,7 @@ mutable struct Snake <: Tile
     orientation::Int
     mirrored::Bool
 end
-Snake() = Snake([5,1], 0, false)
+Snake() = Snake([4,1], 0, false)
 data(tile::Snake) = [1 1 0; 0 1 1] |> x -> rotr90(x, tile.orientation) |>
                 x -> tile.mirrored ? reverse(x * 2 #=green=# , dims=2) : x #=red=#
 
@@ -29,7 +29,7 @@ mutable struct Bar <: Tile
     location::Array{Int}
     orientation::Int
 end
-Bar() = Bar([5,1], 0)
+Bar() = Bar([4,2], 0)
 set_mirrored!(::Bar, b::Bool) = return
 data(tile::Bar) = [1 1 1 1] |> x -> rotr90(x * 6 #=cyan=#, tile.orientation)
 
@@ -38,7 +38,7 @@ mutable struct T <: Tile
     location::Array{Int}
     orientation::Int
 end
-T() = T([5,1], 0)
+T() = T([4,1], 0)
 set_mirrored!(::T, b::Bool) = return
 data(tile::T) = [1 1 1; 0 1 0] |> x -> rotr90(x * 5 #=magenta=#, tile.orientation)
 
